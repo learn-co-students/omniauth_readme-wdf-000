545cc42a8a713ee8fbb94c213f44436327e1a115
Flatiron School <github email address>
2016-09-30 14:18:10 -0400

    done

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+# require 'omniauth-facebook'
+
+# config.omniauth :facebook, FACEBOOK_KEY, FACEBOOK_SECRET
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, "ENV['FACEBOOK_KEY']", ENV['FACEBOOK_SECRET']
+end
