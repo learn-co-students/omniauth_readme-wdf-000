ec3c25dbebdc5ba5842019aff2809e5a2be30bda
HAFIZUR RAHMAN <hafizur.rahmanc@gmail.com>
2016-10-04 15:48:30 -0400

    fixed browsing issue after user creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_or_create_by(uid: auth['uid']) do |u|
+   user = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
       u.email = auth['info']['email']
     end
+   @auth = auth
     session[:user_id] = user.id
   end
 
