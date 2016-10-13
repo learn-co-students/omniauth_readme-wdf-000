9bc4c1076a8d07c55e2f8a316c368e0b0764aaa4
Bibi Khadiza <limakhadiza@yahoo.com>
2016-10-13 15:50:17 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +55,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get '/auth/facebook/callback' => 'sessions#create'
+  root "welcome#home"
+  get '/sessions' => 'sessions#create'
 end
